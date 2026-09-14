b85a8edb0ea152df0f0f4950e787b378fecb7cf6
Vandana-ydv <imvandana24ydv@gmail.com>
2026-09-14 14:36:15 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int[] SortArray(int[] nums) {
+       if(nums==null || nums.Length <= 1) return nums;
+       MergeSort(nums,0,nums.Length-1);
+       return nums;
+    }
+    private void MergeSort(int[] nums,int left, int right){
+        if(left>=right) return;
+        int mid=left+(right-left)/2;
+        MergeSort(nums,left,mid);
+        MergeSort(nums,mid+1,right);
+        Merge(nums,left,mid,right);
+    }
+    private void Merge(int[] nums,int left,int mid,int right){
+        int[] temp=new int[right-left+1];
+        int i=left,j=mid+1,k=0;
+        while(i<=mid && j<=right){
+            if(nums[i]<=nums[j]){
+                temp[k++]=nums[i++];
+           }
+           else{
+            temp[k++]=nums[j++];
+           }
+        }
+        while(i<=mid) temp[k++]=nums[i++];
+        while(j<=right) temp[k++]=nums[j++];
+        for(int a=0;a<temp.Length;a++){
+            nums[left+a]=temp[a];
+        }
+    }
+}
